a38a0f535c4c5e0127cd99911ff165e4e07f0f11
MfrankUg <muhindofrank2001@gmail.com>
2026-07-19 18:52:44 +0300

    application of math library

---
+++ lib.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+const double pi = 3.1415926535897932;
+void main() {
+  double angle = pi / 4;
+  double sine = sin(angle);
+  double cosine = cos(angle);
+  Random random = Random();
+
+  print(angle);
+  print(sine);
+  print(cosine);
+  print(random);
+
+
+}
